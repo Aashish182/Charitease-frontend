3ccc9b13d4e4e5f7412145c459cf31a5b16b501a
Aashish Gupta <aashishgupta99300@gmail.com>
2025-12-03 14:20:07 +0530

    co

--- src/common/index.js
+++ src/common/index.js
@@ -1,195 +1,74 @@
 
-// const backendDomain = "https://charitease-backend.onrender.com";
-
-// const SummaryApi = {
-//     register :{
-//         url: `${backendDomain}/api/Register`,
-//         method : "post"
-//     },
-//     login :{
-//         url: `${backendDomain}/api/Login`,
-//         method : "post"
-//     },
-//     current_user :{
-//         url: `${backendDomain}/api/user-details`,
-//         method : "get"
-//     },
-//     logout_user :{
-//         url: `${backendDomain}/api/userLogout`,
-//         method : "get"
-//     },
-//     createCampaign :{
-//         url: `${backendDomain}/api/createCampaign`,
-//         method : "post"
-//     },
-//     campaigndetail :{
-//         url: `${backendDomain}/api/campaignDetails`,
-//         method : "get"
-//     },
-//     categoryCampaign :{
-//         url: `${backendDomain}/api/get-categoryCampaign`,
-//         method: "get"
-//     },
-//     campaignDetails :{
-//         url: `${backendDomain}/api/campaign-details`,
-//         method: "post"
-//     },
-//     campaignDetailsUser :{
-//         url: `${backendDomain}/api/campaign-details-user`,
-//         method: "post"
-//     },
-//     DeleteCampaign :{
-//         url : `${backendDomain}/api/delete-campaign`,
-//         method : "post"
-//     },
-//     updateCampaign :{
-//         url : `${backendDomain}/api/update-campaign`,
-//         method : "post"
-//     },
-//     updateStory :{
-//         url : `${backendDomain}/api/story-update`,
-//         method : "post"
-//     },
-//     getUpdateStory :{
-//         url : `${backendDomain}/api/get-storyUpdate`,
-//         method : "post"
-//     },
-//     deletestory: {
-//         url : `${backendDomain}/api/delete-story`,
-//         method : 'post'
-//     },
-//     categoryWiseCampaign: {
-//         url : `${backendDomain}/api/category-campaign`,
-//         method : 'post'
-//     },
-//     allUser: {
-//         url: `${backendDomain}/api/all-users`,
-//         method: 'get'
-//     },
-//     updateUser: {
-//         url: `${backendDomain}/api/update-user`,
-//         method: 'post'
-//     },
-//     contactDetail: {
-//         url: `${backendDomain}/api/contact-detail`,
-//         method: "post"
-//     },
-//     allContact: {
-//         url: `${backendDomain}/api/all-contacts`,
-//         method: 'get'
-//     },
-//     addbank: {
-//         url: `${backendDomain}/api/add-bank`,
-//         method: 'post'
-//     },
-//     bankdetail: {
-//         url: `${backendDomain}/api/bank-detail`,
-//         method: 'post'
-//     },
-//     deletebank: {
-//         url: `${backendDomain}/api/delete-bank`,
-//         method: 'post'
-//     },
-//     makeDonation: {
-//         url: `${backendDomain}/api/make-donation`,
-//         method: 'post'
-//     },
-//     allBank: {
-//         url: `${backendDomain}/api/all-bank-detail`,
-//         method: 'get'
-//     },
-//     donationDetail: {
-//         url: `${backendDomain}/api/donation-detail`,
-//         method: 'post'
-//     },
-//     campaignuser: {
-//         url: `${backendDomain}/api/campaign-user`,
-//         method: 'post'
-//     },
-//     withdraw: {
-//         url: `${backendDomain}/api/Withdraw`,
-//         method: 'post'
-//     },
-//     changePassword: {
-//         url: `${backendDomain}/api/change-password`,
-//         method: 'post'
-//     }
-// }
-
-
-// export default SummaryApi;
-
-// Replace this with your Render backend URL
 const backendDomain = "https://charitease-backend.onrender.com";
 
 const SummaryApi = {
-    register: {
-        url: `${backendDomain}/api/register`,
-        method: "post"
+    register :{
+        url: `${backendDomain}/api/Register`,
+        method : "post"
     },
-    login: {
-        url: `${backendDomain}/api/login`,
-        method: "post"
+    login :{
+        url: `${backendDomain}/api/Login`,
+        method : "post"
     },
-    current_user: {
+    current_user :{
         url: `${backendDomain}/api/user-details`,
+        method : "get"
+    },
+    logout_user :{
+        url: `${backendDomain}/api/userLogout`,
+        method : "get"
+    },
+    createCampaign :{
+        url: `${backendDomain}/api/createCampaign`,
+        method : "post"
+    },
+    campaigndetail :{
+        url: `${backendDomain}/api/campaignDetails`,
+        method : "get"
+    },
+    categoryCampaign :{
+        url: `${backendDomain}/api/get-categoryCampaign`,
         method: "get"
     },
-    logout_user: {
-        url: `${backendDomain}/api/user-logout`,
-        method: "get"
-    },
-    createCampaign: {
-        url: `${backendDomain}/api/create-campaign`,
-        method: "post"
-    },
-    campaigndetail: {
+    campaignDetails :{
         url: `${backendDomain}/api/campaign-details`,
-        method: "get"
-    },
-    categoryCampaign: {
-        url: `${backendDomain}/api/get-category-campaign`,
-        method: "get"
-    },
-    campaignDetails: {
-        url: `${backendDomain}/api/campaign-detail`,
         method: "post"
     },
-    campaignDetailsUser: {
-        url: `${backendDomain}/api/campaign-user-detail`,
+    campaignDetailsUser :{
+        url: `${backendDomain}/api/campaign-details-user`,
         method: "post"
     },
-    DeleteCampaign: {
-        url: `${backendDomain}/api/delete-campaign`,
-        method: "post"
+    DeleteCampaign :{
+        url : `${backendDomain}/api/delete-campaign`,
+        method : "post"
     },
-    updateCampaign: {
-        url: `${backendDomain}/api/update-campaign`,
-        method: "post"
+    updateCampaign :{
+        url : `${backendDomain}/api/update-campaign`,
+        method : "post"
     },
-    updateStory: {
-        url: `${backendDomain}/api/story-update`,
-        method: "post"
+    updateStory :{
+        url : `${backendDomain}/api/story-update`,
+        method : "post"
     },
-    getUpdateStory: {
-        url: `${backendDomain}/api/get-story-update`,
-        method: "post"
+    getUpdateStory :{
+        url : `${backendDomain}/api/get-storyUpdate`,
+        method : "post"
     },
     deletestory: {
-        url: `${backendDomain}/api/delete-story`,
-        method: "post"
+        url : `${backendDomain}/api/delete-story`,
+        method : 'post'
     },
     categoryWiseCampaign: {
-        url: `${backendDomain}/api/category-campaign`,
-        method: "post"
+        url : `${backendDomain}/api/category-campaign`,
+        method : 'post'
     },
     allUser: {
         url: `${backendDomain}/api/all-users`,
-        method: "get"
+        method: 'get'
     },
     updateUser: {
         url: `${backendDomain}/api/update-user`,
-        method: "post"
+        method: 'post'
     },
     contactDetail: {
         url: `${backendDomain}/api/contact-detail`,
@@ -197,44 +76,166 @@ const SummaryApi = {
     },
     allContact: {
         url: `${backendDomain}/api/all-contacts`,
-        method: "get"
+        method: 'get'
     },
     addbank: {
         url: `${backendDomain}/api/add-bank`,
-        method: "post"
+        method: 'post'
     },
     bankdetail: {
         url: `${backendDomain}/api/bank-detail`,
-        method: "post"
+        method: 'post'
     },
     deletebank: {
         url: `${backendDomain}/api/delete-bank`,
-        method: "post"
+        method: 'post'
     },
     makeDonation: {
         url: `${backendDomain}/api/make-donation`,
-        method: "post"
+        method: 'post'
     },
     allBank: {
         url: `${backendDomain}/api/all-bank-detail`,
-        method: "get"
+        method: 'get'
     },
     donationDetail: {
         url: `${backendDomain}/api/donation-detail`,
-        method: "post"
+        method: 'post'
     },
     campaignuser: {
         url: `${backendDomain}/api/campaign-user`,
-        method: "post"
+        method: 'post'
     },
     withdraw: {
-        url: `${backendDomain}/api/withdraw`,
-        method: "post"
+        url: `${backendDomain}/api/Withdraw`,
+        method: 'post'
     },
     changePassword: {
         url: `${backendDomain}/api/change-password`,
-        method: "post"
+        method: 'post'
     }
-};
+}
+
 
 export default SummaryApi;
+
+
+
+// const backendDomain = "https://charitease-backend.onrender.com";
+
+// const SummaryApi = {
+//     register: {
+//         url: `${backendDomain}/api/register`,
+//         method: "post"
+//     },
+//     login: {
+//         url: `${backendDomain}/api/login`,
+//         method: "post"
+//     },
+//     current_user: {
+//         url: `${backendDomain}/api/user-details`,
+//         method: "get"
+//     },
+//     logout_user: {
+//         url: `${backendDomain}/api/user-logout`,
+//         method: "get"
+//     },
+//     createCampaign: {
+//         url: `${backendDomain}/api/create-campaign`,
+//         method: "post"
+//     },
+//     campaigndetail: {
+//         url: `${backendDomain}/api/campaign-details`,
+//         method: "get"
+//     },
+//     categoryCampaign: {
+//         url: `${backendDomain}/api/get-category-campaign`,
+//         method: "get"
+//     },
+//     campaignDetails: {
+//         url: `${backendDomain}/api/campaign-detail`,
+//         method: "post"
+//     },
+//     campaignDetailsUser: {
+//         url: `${backendDomain}/api/campaign-user-detail`,
+//         method: "post"
+//     },
+//     DeleteCampaign: {
+//         url: `${backendDomain}/api/delete-campaign`,
+//         method: "post"
+//     },
+//     updateCampaign: {
+//         url: `${backendDomain}/api/update-campaign`,
+//         method: "post"
+//     },
+//     updateStory: {
+//         url: `${backendDomain}/api/story-update`,
+//         method: "post"
+//     },
+//     getUpdateStory: {
+//         url: `${backendDomain}/api/get-story-update`,
+//         method: "post"
+//     },
+//     deletestory: {
+//         url: `${backendDomain}/api/delete-story`,
+//         method: "post"
+//     },
+//     categoryWiseCampaign: {
+//         url: `${backendDomain}/api/category-campaign`,
+//         method: "post"
+//     },
+//     allUser: {
+//         url: `${backendDomain}/api/all-users`,
+//         method: "get"
+//     },
+//     updateUser: {
+//         url: `${backendDomain}/api/update-user`,
+//         method: "post"
+//     },
+//     contactDetail: {
+//         url: `${backendDomain}/api/contact-detail`,
+//         method: "post"
+//     },
+//     allContact: {
+//         url: `${backendDomain}/api/all-contacts`,
+//         method: "get"
+//     },
+//     addbank: {
+//         url: `${backendDomain}/api/add-bank`,
+//         method: "post"
+//     },
+//     bankdetail: {
+//         url: `${backendDomain}/api/bank-detail`,
+//         method: "post"
+//     },
+//     deletebank: {
+//         url: `${backendDomain}/api/delete-bank`,
+//         method: "post"
+//     },
+//     makeDonation: {
+//         url: `${backendDomain}/api/make-donation`,
+//         method: "post"
+//     },
+//     allBank: {
+//         url: `${backendDomain}/api/all-bank-detail`,
+//         method: "get"
+//     },
+//     donationDetail: {
+//         url: `${backendDomain}/api/donation-detail`,
+//         method: "post"
+//     },
+//     campaignuser: {
+//         url: `${backendDomain}/api/campaign-user`,
+//         method: "post"
+//     },
+//     withdraw: {
+//         url: `${backendDomain}/api/withdraw`,
+//         method: "post"
+//     },
+//     changePassword: {
+//         url: `${backendDomain}/api/change-password`,
+//         method: "post"
+//     }
+// };
+
+// export default SummaryApi;
